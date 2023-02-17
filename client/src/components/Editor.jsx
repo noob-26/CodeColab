691d182376ae5072f3acb7e960350d289a1d9417
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2023-02-18 02:01:49 +0530

    fixed some production issues

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,6 +1,6 @@
 import AceEditor from "react-ace";
 
-import "ace-builds/src-min/ace.js";
+import "ace-builds/src-noconflict/ext-language_tools";
 
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/theme-solarized_dark";
@@ -13,28 +13,13 @@ import "ace-builds/src-noconflict/theme-xcode";
 import "ace-builds/src-noconflict/theme-ambiance";
 import "ace-builds/src-noconflict/theme-solarized_light";
 
-import "ace-builds/src-noconflict/ext-language_tools";
-
 import "ace-builds/src-noconflict/snippets/java";
 import "ace-builds/src-noconflict/snippets/c_cpp";
 import "ace-builds/src-noconflict/snippets/python";
-import "ace-builds/src-noconflict/snippets/javascript";
-import "ace-builds/src-noconflict/snippets/rust";
-import "ace-builds/src-noconflict/snippets/kotlin";
-import "ace-builds/src-noconflict/snippets/html";
-import "ace-builds/src-noconflict/snippets/css";
-import "ace-builds/src-noconflict/snippets/markdown";
 
-import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/mode-python";
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/mode-rust";
-import "ace-builds/src-noconflict/mode-kotlin";
-import "ace-builds/src-noconflict/mode-html";
-import "ace-builds/src-noconflict/mode-css";
-import "ace-builds/src-noconflict/mode-markdown";
 
 const Editor = ({
   height,
